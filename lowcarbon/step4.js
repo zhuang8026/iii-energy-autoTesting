d4852f367460ca969e1d7d438c158b11c0b8525e
莊杰翰 Chieh Han Chuang <williamchuang@iii.org.tw>
2024-05-21 16:24:47 +0800

    Fixed - [lowcarbon] step4.js fixed

--- lowcarbon/step4.js
+++ lowcarbon/step4.js
@@ -27,6 +27,23 @@ let arrLink = [];
 let account = 'u0955318835@gmail.com';
 let pwd = 'Enargy17885@';
 
+//正式機 - 登入測試
+async function test_login() {
+  console.log('stage1 - 正式機 - 登入測試');
+
+  await nightmare
+    .goto('https://www.lowcarbon-hems.org.tw', headers) // 进度到当前网址，所以如果想返回，也可以只有.goto()
+    .wait('div.login-page') //等待數秒
+    .wait(() => {
+      return document.querySelector('div.login-page') !== null;
+    }) //确保div.login-page已经出现
+    .wait(2000) //等待數秒
+    .catch((err) => {
+      console.log('ERROR:', err);
+      // throw err;
+    });
+}
+
 //正式機 - 管理用電
 async function drivce() {
   console.log('stage4 - 管理用電');
@@ -116,6 +133,7 @@ async function asyncArray(functionList) {
 
 try {
   asyncArray([
+    test_login,
     drivce, // 用戶行為設定 
     drivceParseHtml, // 畫面掃描 & 檢查資料判斷
     close // 關閉
